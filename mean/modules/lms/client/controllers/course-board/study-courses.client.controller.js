a31e68c906fb9f7aee8edff5346c449e2a1a9dbb
Quang Nguyen <quang.nguyennhat@vcc.vn>
2017-02-13 18:59:06 +0700

    Add LMS course program

--- mean/modules/lms/client/controllers/course-board/study-courses.client.controller.js
+++ mean/modules/lms/client/controllers/course-board/study-courses.client.controller.js
@@ -11,6 +11,9 @@ CoursesStudyController.$inject = ['$scope', '$state', '$window', 'CourseMembersS
 function CoursesStudyController($scope, $state, $window, CourseMembersService,Authentication, EditionSectionsService, course, CoursesService, Notification, edition,treeUtils,$translate ,$q, _) {
     var vm = this;
     vm.course = course;
+    vm.expand =  expand;
+    vm.collapse = collapse;
+    vm.toggleExpand = toggleExpand;
     vm.edition = edition;
     vm.sections = EditionSectionsService.byEdition({editionId:vm.edition._id}, function() {
         vm.sections = _.filter(vm.sections,function(section) {
@@ -22,6 +25,21 @@ function CoursesStudyController($scope, $state, $window, CourseMembersService,Au
         })
      
     });
+    
+    function toggleExpand(node) {
+        if (node.expand)
+            collapse(node);
+        else
+            expand(node);
+    }
+    
+    function expand(node) {
+        treeUtils.expandCourseNode(node,true);
+    }
+    
+    function collapse(node) {
+        treeUtils.expandCourseNode(node,false);
+    }
     
    
 }
