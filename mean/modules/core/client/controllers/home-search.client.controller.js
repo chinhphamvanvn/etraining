fe744466f039c6e0ac65bd374f80780ed13f85c1
thanhvk <thanhvk.w3s@gmail.com>
2017-02-28 08:41:17 +0700

    Home search courses done!

---
+++ mean/modules/core/client/controllers/home-search.client.controller.js
@@ -0,0 +1,38 @@
+/**
+ * Created by thanhvk on 2/28/2017.
+ */
+(function () {
+  'use strict';
+
+  angular
+    .module('core')
+    .controller('HomeSearchController', HomeSearchController);
+
+  HomeSearchController.$inject = ['$scope', '$state', '$stateParams', 'Authentication', 'CoursesService','CourseMembersService','SettingsService', 'AnnoucementsService', '_'];
+
+  function HomeSearchController($scope, $state, $stateParams, Authentication, CoursesService, CourseMembersService,SettingsService, AnnoucementsService, _) {
+    var vm = this;
+    vm.keyword = '';
+
+    vm.user = Authentication.user;
+    vm.authentication = Authentication;
+    vm.gotoWorkspace = gotoWorkspace;
+    SettingsService.registerMode().then(function(data) {
+      vm.registerSetting = data;
+    });
+
+
+    function gotoWorkspace() {
+      if (_.contains(vm.user.roles,'admin'))
+        $state.go('admin.workspace.dashboard');
+      else
+        $state.go('workspace.lms.courses.me');
+    }
+
+    vm.annoucements = AnnoucementsService.listPublished();
+
+    vm.coursesByKeyword = CoursesService.listByKeyword({keyword: $stateParams.keyword}, function() {
+      vm.courses = vm.coursesByKeyword;
+    });
+  }
+}());
