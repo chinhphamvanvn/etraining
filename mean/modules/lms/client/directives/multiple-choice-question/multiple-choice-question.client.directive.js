3eae20d223c2df666700e6d031beb73deab3ba86
Quang Nguyen <quang.nguyennhat@vcc.vn>
2017-03-09 16:45:23 +0700

    Fix ETRAIN-70

--- mean/modules/lms/client/directives/multiple-choice-question/multiple-choice-question.client.directive.js
+++ mean/modules/lms/client/directives/multiple-choice-question/multiple-choice-question.client.directive.js
@@ -13,6 +13,7 @@
           scope: {
               question: "=",
               answer:"=",
+              shuffle:"=",
               showAnswer:"=",
               mode: "="         // edit.view/study, result
           },
@@ -21,7 +22,15 @@
               scope.tinymce_options = fileManagerConfig;
               scope.$watch('question',function() {
                   if (scope.question._id)
-                      scope.question.options = OptionsService.byQuestion({questionId:scope.question._id},function() {
+                      scope.question.options = OptionsService.byQuestion({questionId:scope.question._id},function(options) {
+                          if (scope.mode =='study' && scope.shuffle) {
+                              if (!question.shuffleIndex)
+                                  question.shuffleIndex = Math.floor(Math.random()*options.length);
+                              scope.question.options = [];
+                              for (var i=0;i<options.length;i++)
+                                  scope.question.options.push(options[(question.shuffleIndex + i) % options.length])
+                          }
+                          
                           if (scope.mode !='study' && scope.mode !='result')
                               _.each(scope.question.options,function(option) {
                                   option.selected = _.contains(scope.question.correctOptions,option._id);
