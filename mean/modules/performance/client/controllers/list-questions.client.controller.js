51caa47fda63af0f2a7c3df39a3f37863d09f89b
thanhvk <thanhvk.w3s@gmail.com>
2017-03-12 10:17:41 +0700

    ETRAIN-104 add showmore to list-question

--- mean/modules/performance/client/controllers/list-questions.client.controller.js
+++ mean/modules/performance/client/controllers/list-questions.client.controller.js
@@ -6,7 +6,7 @@
     .controller('QuestionsBankController', QuestionsBankController);
 
   QuestionsBankController.$inject = ['$scope', '$rootScope','$state', 'Authentication','GroupsService','QuestionsService', '$timeout', '$window', 'treeUtils','$translate', '_'];
-  
+
   function QuestionsBankController($scope, $rootScope, $state, Authentication, GroupsService, QuestionsService, $timeout, $window, treeUtils,$translate, _) {
     var vm = this;
     vm.authentication = Authentication;
@@ -14,22 +14,22 @@
     vm.createQuestion = createQuestion;
     vm.remove = remove;
     vm.selectGroup = selectGroup;
-    
+
     function selectGroup(groups) {
         vm.groups = groups;
-        vm.questions  = [];
+        vm.totalQuestions  = [];
         _.each(vm.groups,function(group) {
             QuestionsService.byCategory({groupId:group},function(questions) {
-                vm.questions = vm.questions.concat(questions); 
+                vm.totalQuestions = vm.totalQuestions.concat(questions);
             })
         })
     }
-    
+
     function finishEditQuestionTree() {
         $window.location.reload();
     }
-    
-    
+
+
     function createQuestion(type) {
         if (!vm.groups) {
             UIkit.modal.alert($translate.instant('ERROR.QUESTION.EMPTY_QUESTION_GROUP'));
@@ -41,19 +41,19 @@
         question.$save(function() {
             $state.go('admin.workspace.performance.question.edit',{questionId:question._id})
         });
-        
-         
+
+
     }
-    
+
     function remove(question) {
         UIkit.modal.confirm($translate.instant('COMMON.CONFIRM_PROMPT'), function() {
                 question.$remove(function() {
-                    vm.questions = _.reject(vm.questions ,function(q) {
+                    vm.totalQuestions = _.reject(vm.totalQuestions ,function(q) {
                         return q._id == question._id;
                     })
                 });
             });
     }
-   
+
   }
 }());
