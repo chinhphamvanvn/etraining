fa7b9440c1eed8df380dc01d20f693d56e7b0703
Quang Nguyen <quang.nguyennhat@vcc.vn>
2017-03-19 17:23:03 +1100

    Format source

--- mean/modules/cms/client/config/cms.client.routes.js
+++ mean/modules/cms/client/config/cms.client.routes.js
@@ -1,103 +1,107 @@
-(function () {
-  'use strict';
+(function() {
+	'use strict';
 
-  angular
-    .module('cms')
-    .config(routeConfig);
+	angular
+		.module('cms')
+		.config(routeConfig);
 
-  routeConfig.$inject = ['$stateProvider'];
+	routeConfig.$inject = [ '$stateProvider' ];
 
-  function routeConfig($stateProvider) {
-    $stateProvider
-      .state('admin.workspace.cms', {
-        abstract: true,
-        url: '/cms',
-        template: '<ui-view/>'
-      })
-      .state('admin.workspace.cms.courses', {
-        abstract: true,
-        url: '/courses',
-        template: '<ui-view/>'
-      })
-      .state('admin.workspace.cms.courses.list', {
-        url: '/list',
-        templateUrl: '/modules/cms/client/views/list-courses.client.view.html',
-        controller: 'CmsCoursesListController',
-        controllerAs: 'vm',
-        data: {
-            roles: [ 'admin'],
-        }
-      })
-      .state('admin.workspace.cms.courses.create', {
-        url: '/create',
-        templateUrl: '/modules/cms/client/views/form-course.client.view.html',
-        controller: 'CoursesController',
-        controllerAs: 'vm',
-        resolve: {
-          courseResolve: newCourse
-        },
-        data: {
-          roles: [ 'admin'],
-        }
-      })
-      .state('admin.workspace.cms.courses.edit', {
-        url: '/edit/:courseId',
-        templateUrl: '/modules/cms/client/views/form-course.client.view.html',
-        controller: 'CoursesController',
-        controllerAs: 'vm',
-        resolve: {
-          courseResolve: getCourse
-        },
-        data: {
-          roles: ['admin'],
-        }
-      })
-      .state('admin.workspace.cms.courses.view', {
-        url: '/view/:courseId',
-        templateUrl: '/modules/cms/client/views/view-course.client.view.html',
-        controller: 'CourseViewController',
-        controllerAs: 'vm',
-        resolve: {
-          courseResolve: getCourse
-        },
-        data: {
-          roles: ['admin'],
-        }
-      })
-      .state('admin.workspace.cms.course-members', {
-        url: '/members/:courseId',
-        templateUrl: '/modules/cms/client/views/list-course.members.client.view.html',
-        controller: 'CourseMembersController',
-        controllerAs: 'vm',
-        resolve: {
-          courseResolve: getCourse,
-          editionResolve: getEdition
-        },
-        data: {
-            roles: [ 'admin'],
-        }
-      });
-  }
-  
-  getEdition.$inject = ['$stateParams', 'CourseEditionsService'];
+	function routeConfig($stateProvider) {
+		$stateProvider
+			.state('admin.workspace.cms', {
+				abstract : true,
+				url : '/cms',
+				template : '<ui-view/>'
+			})
+			.state('admin.workspace.cms.courses', {
+				abstract : true,
+				url : '/courses',
+				template : '<ui-view/>'
+			})
+			.state('admin.workspace.cms.courses.list', {
+				url : '/list',
+				templateUrl : '/modules/cms/client/views/list-courses.client.view.html',
+				controller : 'CmsCoursesListController',
+				controllerAs : 'vm',
+				data : {
+					roles : [ 'admin' ],
+				}
+			})
+			.state('admin.workspace.cms.courses.create', {
+				url : '/create',
+				templateUrl : '/modules/cms/client/views/form-course.client.view.html',
+				controller : 'CoursesController',
+				controllerAs : 'vm',
+				resolve : {
+					courseResolve : newCourse
+				},
+				data : {
+					roles : [ 'admin' ],
+				}
+			})
+			.state('admin.workspace.cms.courses.edit', {
+				url : '/edit/:courseId',
+				templateUrl : '/modules/cms/client/views/form-course.client.view.html',
+				controller : 'CoursesController',
+				controllerAs : 'vm',
+				resolve : {
+					courseResolve : getCourse
+				},
+				data : {
+					roles : [ 'admin' ],
+				}
+			})
+			.state('admin.workspace.cms.courses.view', {
+				url : '/view/:courseId',
+				templateUrl : '/modules/cms/client/views/view-course.client.view.html',
+				controller : 'CourseViewController',
+				controllerAs : 'vm',
+				resolve : {
+					courseResolve : getCourse
+				},
+				data : {
+					roles : [ 'admin' ],
+				}
+			})
+			.state('admin.workspace.cms.course-members', {
+				url : '/members/:courseId',
+				templateUrl : '/modules/cms/client/views/list-course.members.client.view.html',
+				controller : 'CourseMembersController',
+				controllerAs : 'vm',
+				resolve : {
+					courseResolve : getCourse,
+					editionResolve : getEdition
+				},
+				data : {
+					roles : [ 'admin' ],
+				}
+			});
+	}
 
-  function getEdition($stateParams, CourseEditionsService) {
-      if ($stateParams.editionId)
-          return CourseEditionsService.get({editionId:$stateParams.editionId}).$promise;
-      return  CourseEditionsService.byCourse({courseId:$stateParams.courseId}).$promise;
-  }
+	getEdition.$inject = [ '$stateParams', 'CourseEditionsService' ];
 
-  getCourse.$inject = ['$stateParams', 'CoursesService'];
+	function getEdition($stateParams, CourseEditionsService) {
+		if ($stateParams.editionId)
+			return CourseEditionsService.get({
+				editionId : $stateParams.editionId
+			}).$promise;
+		return CourseEditionsService.byCourse({
+			courseId : $stateParams.courseId
+		}).$promise;
+	}
 
-  function getCourse($stateParams, CoursesService) {
-    return CoursesService.get({
-      courseId: $stateParams.courseId
-    }).$promise;
-  }
+	getCourse.$inject = [ '$stateParams', 'CoursesService' ];
 
-  newCourse.$inject = ['CoursesService'];
+	function getCourse($stateParams, CoursesService) {
+		return CoursesService.get({
+			courseId : $stateParams.courseId
+		}).$promise;
+	}
 
-  function newCourse(CoursesService) {
-    return new CoursesService();
-  }
+	newCourse.$inject = [ 'CoursesService' ];
+
+	function newCourse(CoursesService) {
+		return new CoursesService();
+	}
 }());
--- mean/modules/lms/client/controllers/teacher/gradeboard-courses.client.controller.js
+++ mean/modules/lms/client/controllers/teacher/gradeboard-courses.client.controller.js
@@ -1,136 +1,143 @@
-(function () {
-  'use strict';
+(function() {
+	'use strict';
 
-// Courses controller
-  angular
-    .module('lms')
-    .controller('CoursesGradeboardController', CoursesGradeboardController);
+	// Courses controller
+	angular
+		.module('lms')
+		.controller('CoursesGradeboardController', CoursesGradeboardController);
 
-  CoursesGradeboardController.$inject = ['$scope', '$state', '$window', 'Authentication', '$timeout', 'editionResolve', 'courseResolve', 'memberResolve', 'gradeResolve', 'userResolve', 'Notification', 'CourseEditionsService', 'CertificatesService', 'CourseMembersService', 'EditionSectionsService', 'treeUtils', 'ExamsService', 'AttemptsService', 'QuestionsService','CompetencyAchievementsService', 'courseUtils', '$q', '$translate', '_'];
+	CoursesGradeboardController.$inject = [ '$scope', '$state', '$window', 'Authentication', '$timeout', 'editionResolve', 'courseResolve', 'memberResolve', 'gradeResolve', 'userResolve', 'Notification', 'CourseEditionsService', 'CertificatesService', 'CourseMembersService', 'EditionSectionsService', 'treeUtils', 'ExamsService', 'AttemptsService', 'QuestionsService', 'CompetencyAchievementsService', 'courseUtils', '$q', '$translate', '_' ];
 
-  function CoursesGradeboardController($scope, $state, $window, Authentication, $timeout, edition, course, member, gradescheme, user, Notification, CourseEditionsService, CertificatesService, CourseMembersService, EditionSectionsService, treeUtils, ExamsService, AttemptsService, QuestionsService,CompetencyAchievementsService, courseUtils, $q, $translate, _) {
-    var vm = this;
-    vm.course = course;
-    vm.edition = edition;
-    vm.member = member;
-    vm.certify = certify;
-    vm.user = user;
-    vm.gradescheme = gradescheme;
-    vm.scoreMap = {};
+	function CoursesGradeboardController($scope, $state, $window, Authentication, $timeout, edition, course, member, gradescheme, user, Notification, CourseEditionsService, CertificatesService, CourseMembersService, EditionSectionsService, treeUtils, ExamsService, AttemptsService, QuestionsService, CompetencyAchievementsService, courseUtils, $q, $translate, _) {
+		var vm = this;
+		vm.course = course;
+		vm.edition = edition;
+		vm.member = member;
+		vm.certify = certify;
+		vm.user = user;
+		vm.gradescheme = gradescheme;
+		vm.scoreMap = {};
 
-    function examPromise() {
-      return EditionSectionsService.byEdition({editionId: vm.edition._id}, function (sections) {
-      }).$promise;
-    }
+		function examPromise() {
+			return EditionSectionsService.byEdition({
+				editionId : vm.edition._id
+			}, function(sections) {}).$promise;
+		}
 
-    function membersPromise() {
-      return CourseMembersService.byCourse({courseId: vm.course._id}, function () {
-      }).$promise;
-    }
+		function membersPromise() {
+			return CourseMembersService.byCourse({
+				courseId : vm.course._id
+			}, function() {}).$promise;
+		}
 
-    membersPromise().then(function (members) {
-      vm.members = _.filter(members, function (m) {
-        return m.role == 'student';
-      });
+		membersPromise().then(function(members) {
+			vm.members = _.filter(members, function(m) {
+				return m.role == 'student';
+			});
 
-      _.each(vm.members, function (member) {
-        CertificatesService.byMember({memberId: member._id}, function (certificate) {
-          member.certificate = certificate;
-        }, function () {
-          member.certificate = null;
-        })
-      })
-    }).then(function () {
-      return examPromise();
-    }).then(function (sections) { // Get examList information
-      vm.sections = sections;
-      
-      var nodes = treeUtils.buildCourseTree(sections);
-      vm.examList = treeUtils.buildCourseListInOrder(nodes);
+			_.each(vm.members, function(member) {
+				CertificatesService.byMember({
+					memberId : member._id
+				}, function(certificate) {
+					member.certificate = certificate;
+				}, function() {
+					member.certificate = null;
+				})
+			})
+		}).then(function() {
+			return examPromise();
+		}).then(function(sections) { // Get examList information
+			vm.sections = sections;
 
-      vm.examList = _.filter(vm.examList, function (node) {
-        return node.data.hasContent && node.data.contentType == 'test';
-      });
+			var nodes = treeUtils.buildCourseTree(sections);
+			vm.examList = treeUtils.buildCourseListInOrder(nodes);
 
-      _.each(vm.examList, function (node) {
-        node.min = '0';
-        node.max = '100';
-        var mark = _.find(vm.gradescheme.marks, function (m) {
-          return m.quiz == node.data._id;
-        });
-        if (mark) {
-          node.checked = true;
-          node.weight = mark.weight;
-          node.max = node.weight;
-        } else {
-          node.weight = 0;
-          node.checked = false;
-        }
-        vm.total += node.weight;
-      });
-    }).then(function () {// Get result of exam for each student
-      vm.members.reduce(function (prev, curr) {
-        return prev.then(function () {
-          return courseUtils.memberScoreByCourse(curr._id, vm.edition._id).then(function (scores) {
-              vm.scoreMap[curr._id] = {totalScore:scores.totalScore,scores:scores.scores}
-          });
-        });
-      }, $q.resolve()).then(function () { // Export to csv file
-        var pass = $translate.instant('COMMON.PASS'),
-          fall = $translate.instant('COMMON.FAIL'),
-          displayName = $translate.instant('MODEL.USER.DISPLAY_NAME'),
-          totalScore = $translate.instant('PAGE.LMS.MY_COURSES.GRADE_SCHEME'),
-          result = $translate.instant('PAGE.LMS.MY_COURSES.COURSE_GRADE.EXAM_RESULT');
-        vm.csvArray = [];
-        vm.csvHeader = [];
+			vm.examList = _.filter(vm.examList, function(node) {
+				return node.data.hasContent && node.data.contentType == 'test';
+			});
 
-        vm.csvHeader.push(displayName);
-        vm.examList.map(function (exam) {
-          vm.csvHeader.push(exam.data.name);
-        });
-        vm.csvHeader.push(totalScore);
-        vm.csvHeader.push(result);
+			_.each(vm.examList, function(node) {
+				node.min = '0';
+				node.max = '100';
+				var mark = _.find(vm.gradescheme.marks, function(m) {
+					return m.quiz == node.data._id;
+				});
+				if (mark) {
+					node.checked = true;
+					node.weight = mark.weight;
+					node.max = node.weight;
+				} else {
+					node.weight = 0;
+					node.checked = false;
+				}
+				vm.total += node.weight;
+			});
+		}).then(function() { // Get result of exam for each student
+			vm.members.reduce(function(prev, curr) {
+				return prev.then(function() {
+					return courseUtils.memberScoreByCourse(curr._id, vm.edition._id).then(function(scores) {
+						vm.scoreMap[curr._id] = {
+							totalScore : scores.totalScore,
+							scores : scores.scores
+						}
+					});
+				});
+			}, $q.resolve()).then(function() { // Export to csv file
+				var pass = $translate.instant('COMMON.PASS'),
+					fall = $translate.instant('COMMON.FAIL'),
+					displayName = $translate.instant('MODEL.USER.DISPLAY_NAME'),
+					totalScore = $translate.instant('PAGE.LMS.MY_COURSES.GRADE_SCHEME'),
+					result = $translate.instant('PAGE.LMS.MY_COURSES.COURSE_GRADE.EXAM_RESULT');
+				vm.csvArray = [];
+				vm.csvHeader = [];
 
-        vm.members.map(function (member) {
-          var csvObj = {};
-          csvObj[0 + 'name'] = member.member.displayName;
-          vm.scoreMap[member._id].scores.map(function (score, index) {
-            csvObj['score_' + index] = score.correctPercent;
-          });
-          csvObj.totalScore = vm.scoreMap[member._id].totalScore;
-          csvObj.result = (vm.scoreMap[member._id].totalScore >= vm.gradescheme.benchmark) ? pass : fall;
-          vm.csvArray.push(csvObj);
-        });
-      });
-    });
+				vm.csvHeader.push(displayName);
+				vm.examList.map(function(exam) {
+					vm.csvHeader.push(exam.data.name);
+				});
+				vm.csvHeader.push(totalScore);
+				vm.csvHeader.push(result);
 
-    function certify(member) {
-      var modal = UIkit.modal.blockUI('<div class=\'uk-text-center\'>Processing...<br/><img class=\'uk-margin-top\' src=\'/assets/img/spinners/spinner.gif\' alt=\'\'>');
-      var certificate = new CertificatesService();
-      certificate.member = member._id;
-      certificate.course = vm.course._id;
-      certificate.edition = vm.edition._id;
-      certificate.issueDate = new Date();
-      certificate.authorizer = vm.user._id;
-      certificate.$save(function () {
-        member.enrollmentStatus ='completed';
-        member.$complete(function() {
-            member.certificate = certificate;
-        });        
-        if (vm.course.competency) {
-            var achievement = new CompetencyAchievementsService();
-            achievement.achiever = member.member._id;
-            achievement.competency = vm.course.competency;
-            achievement.source = 'course';
-            achievement.issueBy = new Date();
-            achievement.granter = vm.user._id;
-            achievement.$save(function() {
-                modal.hide();
-            });
-        } else
-            modal.hide();
-        
-      });
-    }
-  }
+				vm.members.map(function(member) {
+					var csvObj = {};
+					csvObj[0 + 'name'] = member.member.displayName;
+					vm.scoreMap[member._id].scores.map(function(score, index) {
+						csvObj['score_' + index] = score.correctPercent;
+					});
+					csvObj.totalScore = vm.scoreMap[member._id].totalScore;
+					csvObj.result = (vm.scoreMap[member._id].totalScore >= vm.gradescheme.benchmark) ? pass : fall;
+					vm.csvArray.push(csvObj);
+				});
+			});
+		});
+
+		function certify(member) {
+			var modal = UIkit.modal.blockUI('<div class=\'uk-text-center\'>Processing...<br/><img class=\'uk-margin-top\' src=\'/assets/img/spinners/spinner.gif\' alt=\'\'>');
+			var certificate = new CertificatesService();
+			certificate.member = member._id;
+			certificate.course = vm.course._id;
+			certificate.edition = vm.edition._id;
+			certificate.issueDate = new Date();
+			certificate.authorizer = vm.user._id;
+			certificate.$save(function() {
+				member.enrollmentStatus = 'completed';
+				member.$complete(function() {
+					member.certificate = certificate;
+				});
+				if (vm.course.competency) {
+					var achievement = new CompetencyAchievementsService();
+					achievement.achiever = member.member._id;
+					achievement.competency = vm.course.competency;
+					achievement.source = 'course';
+					achievement.issueBy = new Date();
+					achievement.granter = vm.user._id;
+					achievement.$save(function() {
+						modal.hide();
+					});
+				} else
+					modal.hide();
+
+			});
+		}
+	}
 }());
