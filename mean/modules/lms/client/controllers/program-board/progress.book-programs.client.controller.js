6216b6afa80c6b89ca52ecccd261a9b7222a6b02
thanhvk <thanhvk.w3s@gmail.com>
2017-03-30 17:11:29 +0700

    ETRAIN-141 to review

--- mean/modules/lms/client/controllers/program-board/progress.book-programs.client.controller.js
+++ mean/modules/lms/client/controllers/program-board/progress.book-programs.client.controller.js
@@ -1,18 +1,17 @@
-(function() {
+(function () {
     'use strict';
 
 // Courses controller
-angular
-    .module('lms')
-    .controller('ProgramProgressController', ProgramProgressController);
+    angular
+        .module('lms')
+        .controller('ProgramProgressController', ProgramProgressController);
 
-ProgramProgressController.$inject = ['$scope', '$state', '$window', 'Authentication', '$timeout', 'programResolve','programMemberResolve','Notification','_'];
+    ProgramProgressController.$inject = ['$scope', '$state', '$window', 'Authentication', '$timeout', 'programResolve', 'programMemberResolve', 'Notification', '_'];
 
-function ProgramProgressController($scope, $state, $window, Authentication, $timeout, program, member,  Notification, _) {
-    var vm = this;
-    vm.authentication = Authentication;
-    vm.member = member;
-    vm.program = program;
-    
-}
+    function ProgramProgressController($scope, $state, $window, Authentication, $timeout, program, member, Notification, _) {
+        var vm = this;
+        vm.authentication = Authentication;
+        vm.member = member;
+        vm.program = program;
+    }
 }());
--- mean/modules/lms/client/directives/program-progress-book/program-progress-book.client.directive.js
+++ mean/modules/lms/client/directives/program-progress-book/program-progress-book.client.directive.js
@@ -1,36 +1,37 @@
 (function () {
-  'use strict';
+    'use strict';
 
-  // Focus the element on page load
-  // Unless the user is on a small device, because this could obscure the page with a keyboard
+    // Focus the element on page load
+    // Unless the user is on a small device, because this could obscure the page with a keyboard
 
-  angular.module('lms')
-    .directive('programProgressBook', ['CourseProgramsService','CoursesService','CourseMembersService', 'ProgramMembersService','courseUtils', '_', programProgressBook]);
+    angular.module('lms')
+        .directive('programProgressBook', ['CourseProgramsService', 'CoursesService', 'CourseMembersService', 'ProgramMembersService', 'courseUtils', '_', programProgressBook]);
 
-  function programProgressBook(CourseProgramsService,CoursesService,CourseMembersService,ProgramMembersService, courseUtils, _) {
+    function programProgressBook(CourseProgramsService, CoursesService, CourseMembersService, ProgramMembersService, courseUtils, _) {
 
-      return {
-          scope: {
-              program: "=",
-              member: "=",
-          },
-          templateUrl:'/modules/lms/client/directives/program-progress-book/program-progress-book.directive.client.view.html',
-          link: function (scope, element, attributes) {
-             scope.courses = [];
-             _.each(scope.program.courses,function(courseId) {
-            	 CoursesService.get({courseId:courseId},function(course) {
-            		 scope.courses.push(course);
-            		 course.percentage = 0;
-            		 course.member = CourseMembersService.byUserAndCourse({userId:member.member,courseId:courseId},function() {
-            			 courseUtils.memberProgress(course.member._id,course.member.edition._id,function(progress) {
-            				 course.percentage = progress;
-            			 })
-            		 });
-            		  
-            	 })
-             })
-              
-          }
-      }
-  }
+        return {
+            scope: {
+                program: "=",
+                member: "="
+            },
+            templateUrl: '/modules/lms/client/directives/program-progress-book/program-progress-book.directive.client.view.html',
+            link: function (scope, element, attributes) {
+                scope.courses = [];
+                _.each(scope.program.courses, function (courseId) {
+                    CoursesService.get({courseId: courseId}, function (course) {
+                        scope.courses.push(course);
+                        course.percentage = 0;
+                        course.member = CourseMembersService.byUserAndCourse({
+                            userId: scope.member.member._id,
+                            courseId: course._id
+                        }, function () {
+                            courseUtils.memberProgress(course.member._id, course.member.edition, function (progress) {
+                                course.percentage = progress;
+                            })
+                        });
+                    })
+                })
+            }
+        }
+    }
 }());
