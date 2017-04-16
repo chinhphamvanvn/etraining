5e48848f267fdedd16a4c556bcf3134200ae713c
Quang Nguyen <technicalmanager@gmail.com>
2017-04-16 14:14:24 +1000

    Add drag drop question

--- mean/src/client/lms/directives/questions/associative-question/associative-question.client.directive.js
+++ mean/src/client/lms/directives/questions/associative-question/associative-question.client.directive.js
@@ -35,7 +35,147 @@
       },
       templateUrl: '/src/client/lms/directives/questions/associative-question/associative-question.directive.client.view.html',
       link: function(scope, element, attributes) {
+        scope.tinymce_options = fileManagerConfig;
+
+        scope.$watch('question', function() {
+          if (scope.question._id)
+            scope.question.options = OptionsService.byQuestion({
+              questionId: scope.question._id
+            }, function() {
+              updateTargetList();
+              enterMode();
+            });
+          else {
+            scope.question.options = [];
+            enterMode();
+          }
+        });
+
+        function enterMode() {
+          switch (scope.mode) {
+            case 'edit':
+              markCorrectAssoc();
+              scope.addSourceOption = addSourceOption;
+              scope.addTargetOption = addTargetOption;
+              scope.removeOption = removeOption;
+              scope.selectAssoc = updateCorrectAssoc;
+              break;
+            case 'study':
+              preprocessQuestionContent();
+              rearrangeOptions();
+              markAnswerAssoc();
+              break;
+            case 'view':
+              preprocessQuestionContent();
+              markCorrectAssoc();
+              break;
+            case 'result':
+              preprocessQuestionContent();
+              markAnswerAssoc();
+              break;
+          }
+        }
+
+        function rearrangeOptions() {
+          if (scope.shuffle) {
+            var options = scope.question.options;
+            if (!scope.question.shuffleIndex)
+              scope.question.shuffleIndex = Math.floor(Math.random() * options.length);
+            scope.question.options = [];
+            for (var i = 0; i < options.length; i++)
+              scope.question.options.push(options[(scope.question.shuffleIndex + i) % options.length]);
+          } else
+            scope.question.options = _.sortBy(scope.question.options, 'order');
+        }
+
+        function markCorrectAssoc() {
+          _.each(scope.question.optionMappings, function(assoc) {
+            var option = _.find(scope.question.options, function(obj) {
+              return obj._id === assoc.source;
+            });
+            option.target = assoc.target;
+          });
+        }
+
+        function markAnswerAssoc() {
+          if (scope.answer) {
+            _.each(scope.question.options, function(option) {
+              var assoc = _.find(scope.answer.optionMappings, function(obj) {
+                return obj.source === option._id;
+              });
+              if (assoc)
+                option.target = assoc.target;
+            });
+          }
+        }
+
+        function addSourceOption() {
+          var option = new OptionsService();
+          if (scope.question.options.length === 0)
+            option.order = scope.question.options.length + 1;
+          else
+            option.order = _.max(scope.question.options, function(object) { return object.order;}).order + 1;
+          option.question = scope.question._id;
+          option.group = 'source';
+          option.$save(function() {
+            scope.question.options.push(option);
+          });
+        }
+
+        function addTargetOption() {
+          var option = new OptionsService();
+          if (scope.question.options.length === 0)
+            option.order = scope.question.options.length + 1;
+          else
+            option.order = _.max(scope.question.options, function(object) { return object.order;}).order + 1;
+          option.question = scope.question._id;
+          option.group = 'target';
+          option.$save(function() {
+            scope.question.options.push(option);
+            updateTargetList();
+          });
+        }
+
+
+        function updateCorrectAssoc() {
+          var sourceOptions = _.filter(scope.question.options, function(obj) {
+            return obj.group === 'source' && obj.target;
+          });
+          scope.question.optionMappings = _.map(sourceOptions, function(obj) {
+            return {
+              source: obj._id,
+              target: obj.target
+            };
+          });
+        }
+
+
+        function preprocessQuestionContent() {
+        }
+
+
+        function removeOption(option) {
+          scope.question.options = _.reject(scope.question.options, function(o) {
+            return o._id === option._id;
+          });
+          updateTargetList();
+          scope.question.optionMappings = _.reject(scope.question.optionMappings, function(o) {
+            return o.source === option._id || o.target === option._id;
+          });
+          OptionsService.delete({
+            optionId: option._id
+          });
+        }
+
+
         function updateTargetList() {
+          scope.targetConfig = {
+            maxItems: 1,
+            valueField: 'value',
+            labelField: 'title',
+            searchField: 'title',
+            create: false
+          };
           var targetOptions = _.filter(scope.question.options, function(obj) {
             return obj.group === 'target';
           });
@@ -51,126 +191,12 @@
           });
         }
 
-        scope.tinymce_options = fileManagerConfig;
-        scope.targetConfig = {
-          maxItems: 1,
-          valueField: 'value',
-          labelField: 'title',
-          searchField: 'title',
-          create: false
-        };
-
-        scope.$watch('question', function() {
-          if (scope.question._id)
-            scope.question.options = OptionsService.byQuestion({
-              questionId: scope.question._id
-            }, function(options) {
-              var option;
-              updateTargetList();
-              // shuffle the question option
-              if (scope.mode === 'study' && scope.shuffle) {
-                if (!scope.question.shuffleIndex)
-                  scope.question.shuffleIndex = Math.floor(Math.random() * options.length);
-                scope.question.options = [];
-                for (var i = 0; i < options.length; i++)
-                  scope.question.options.push(options[(scope.question.shuffleIndex + i) % options.length]);
-              }
-
-              if (scope.mode === 'study' && !scope.shuffle) {
-                scope.question.options = _.sortBy(scope.question.options, 'order');
-              }
-              // initialize the option
-              if (scope.mode !== 'study' && scope.mode !== 'result') {
-                _.each(scope.question.optionMappings, function(assoc) {
-                  option = _.find(scope.question.options, function(obj) {
-                    return obj._id === assoc.source;
-                  });
-                  option.target = assoc.target;
-                });
-              } else {
-                if (scope.answer) {
-                  _.each(scope.question.options, function(option) {
-                    var assoc = _.find(scope.answer.optionMappings, function(obj) {
-                      return obj.source === option._id;
-                    });
-                    if (assoc)
-                      option.target = assoc.target;
-                  });
-                }
-              }
-            });
-          else
-            scope.question.options = [];
-        });
-
-        scope.getOption = function(id) {
+        scope.optionById = function(id) {
           return _.find(scope.question.options, function(option) {
             return option._id === id;
           });
         };
 
-        scope.addSourceOption = function() {
-          var option = new OptionsService();
-          if (scope.question.options.length === 0)
-            option.order = 1;
-          else
-            option.order = _.max(scope.question.options, function(object) { return object.order;}).order + 1;
-          option.question = scope.question._id;
-          option.group = 'source';
-          option.$save(function() {
-            scope.question.options.push(option);
-          });
-        };
-
-        scope.addTargetOption = function() {
-          var option = new OptionsService();
-          if (scope.question.options.length === 0) {
-            option.order = 1;
-          } else {
-            option.order = _.max(scope.question.options, function(object) { return object.order;}).order + 1;
-          }
-          option.question = scope.question._id;
-          option.group = 'target';
-          option.$save(function() {
-            scope.question.options.push(option);
-            updateTargetList();
-          });
-        };
-
-        scope.selectAssociation = function() {
-          if (scope.mode === 'edit') {
-            var sourceOptions = _.filter(scope.question.options, function(obj) {
-              return obj.group === 'source' && obj.target;
-            });
-            scope.question.optionMappings = _.map(sourceOptions, function(obj) {
-              return {
-                source: obj._id,
-                target: obj.target
-              };
-            });
-          }
-        };
-
-
-        scope.translateContent = function() {
-          return scope.question.description;
-        };
-
-        scope.removeOption = function(option) {
-          if (option._id) {
-            OptionsService.delete({
-              optionId: option._id
-            }, function() {
-              scope.question.options = _.reject(scope.question.options, function(o) {
-                return o._id === option._id;
-              });
-              updateTargetList();
-              scope.question.optionMappings = _.reject(scope.question.optionMappings, function(o) {
-                return o.source === option._id || o.target === option._id;
-              });
-            });
-          }
-        };
       }
     };
   }
--- mean/src/client/lms/directives/questions/true-false-question/true-false-question-view.client.directive.js
+++ mean/src/client/lms/directives/questions/true-false-question/true-false-question-view.client.directive.js
@@ -18,75 +18,124 @@
       link: function(scope, element, attributes) {
         scope.tinymce_options = fileManagerConfig;
         scope.$watch('question', function() {
-          if (scope.question._id) {
+          if (scope.question._id)
             scope.question.options = OptionsService.byQuestion({
               questionId: scope.question._id
-            }, function(options) {
-              if (scope.mode === 'study' && scope.shuffle) {
-                if (!scope.question.shuffleIndex) {
-                  scope.question.shuffleIndex = Math.floor(Math.random() * options.length);
-                }
-                scope.question.options = [];
-                for (var i = 0; i < options.length; i++) {
-                  scope.question.options.push(options[(scope.question.shuffleIndex + i) % options.length]);
-                }
-              }
-
-              if (scope.mode === 'study' && !scope.shuffle) {
-                scope.question.options = _.sortBy(scope.question.options, 'order');
-              }
-
-              if (scope.question.options.length) {
-                if (scope.mode !== 'study' && scope.mode !== 'result') {
-                  _.each(scope.question.options, function(option) {
-                    option.selected = _.contains(scope.question.correctOptions, option._id);
-                  });
-                } else {
-                  if (scope.answer) {
-                    _.each(scope.question.options, function(option) {
-                      option.selected = _.contains(scope.answer.options, option._id);
-                    });
-                  }
-                  _.each(scope.question.options, function(option) {
-                    option.isCorrect = _.contains(scope.question.correctOptions, option._id);
-                  });
-                }
-              } else {
-                var option1 = new OptionsService();
-                option1.order = 1;
-                option1.question = scope.question._id;
-                option1.content = $translate.instant('COMMON.TRUE');
-                option1.$save(function() {
-                  scope.question.options.push(option1);
-                });
-                var option2 = new OptionsService();
-                option2.order = 2;
-                option2.question = scope.question._id;
-                option2.content = $translate.instant('COMMON.FALSE');
-                option2.$save(function() {
-                  scope.question.options.push(option2);
-                });
-              }
+            }, function() {
+              enterMode();
             });
+          else {
+            scope.question.options = [];
+            var option1 = new OptionsService();
+            option1.order = 1;
+            option1.question = scope.question._id;
+            option1.content = $translate.instant('COMMON.TRUE');
+            option1.$save(function() {
+              scope.question.options.push(option1);
+            });
+            var option2 = new OptionsService();
+            option2.order = 2;
+            option2.question = scope.question._id;
+            option2.content = $translate.instant('COMMON.FALSE');
+            option2.$save(function() {
+              scope.question.options.push(option2);
+            });
+            enterMode();
           }
         });
 
-        scope.translateContent = function() {
-          return scope.question.description;
-        };
+        function enterMode() {
+          switch (scope.mode) {
+            case 'edit':
+              markCorrectOption();
+              scope.selectOption = function(option) {
+                selectSingleOption(option);
+                updateCorrectOption(option);
+              };
+              break;
+            case 'study':
+              preprocessQuestionContent();
+              rearrangeOptions();
+              markAnswerOption();
+              scope.selectOption = selectSingleOption;
+              break;
+            case 'view':
+              preprocessQuestionContent();
+              markCorrectOption();
+              break;
+            case 'result':
+              preprocessQuestionContent();
+              markAnswerOption();
+              break;
+          }
+        }
 
-        scope.selectOption = function(option) {
+        function rearrangeOptions() {
+          if (scope.shuffle) {
+            var options = scope.question.options;
+            if (!scope.question.shuffleIndex)
+              scope.question.shuffleIndex = Math.floor(Math.random() * options.length);
+            scope.question.options = [];
+            for (var i = 0; i < options.length; i++)
+              scope.question.options.push(options[(scope.question.shuffleIndex + i) % options.length]);
+          } else
+            scope.question.options = _.sortBy(scope.question.options, 'order');
+        }
+
+        function markCorrectOption() {
+          _.each(scope.question.options, function(option) {
+            option.selected = _.contains(scope.question.correctOptions, option._id);
+          });
+        }
+
+        function markAnswerOption() {
+          if (scope.answer) {
+            _.each(scope.question.options, function(option) {
+              option.selected = _.contains(scope.answer.options, option._id);
+            });
+          }
+        }
+
+        function addOption() {
+          var option = new OptionsService();
+          if (scope.question.options.length === 0)
+            option.order = scope.question.options.length + 1;
+          else
+            option.order = _.max(scope.question.options, function(object) { return object.order;}).order + 1;
+          option.question = scope.question._id;
+          option.$save(function() {
+            scope.question.options.push(option);
+          });
+        }
+
+        function selectSingleOption(option) {
           _.each(scope.question.options, function(obj) {
             if (obj._id !== option._id)
               obj.selected = false;
           });
-          if (scope.mode === 'edit') {
-            var correctOptions = _.filter(scope.question.options, function(option) {
-              return option.selected;
-            });
-            scope.question.correctOptions = _.pluck(correctOptions, '_id');
-          }
-        };
+        }
+
+        function updateCorrectOption(option) {
+          var correctOptions = _.filter(scope.question.options, function(option) {
+            return option.selected;
+          });
+          scope.question.correctOptions = _.pluck(correctOptions, '_id');
+        }
+
+        function preprocessQuestionContent() {
+        }
+
+        function removeOption(option) {
+          scope.question.options = _.reject(scope.question.options, function(o) {
+            return o._id === option._id;
+          });
+          scope.question.correctOptions = _.reject(scope.question.correctOptions, function(o) {
+            return o === option._id;
+          });
+          OptionsService.delete({
+            optionId: option._id
+          });
+        }
       }
     };
   }
